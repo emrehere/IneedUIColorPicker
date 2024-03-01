d58667c388f5621ed6b236a6f9e6b1c4eabbafb2
emrehere <emrah.himself@hotmail.com>
2024-03-01 15:41:35 +0300

    cleaning in server

--- server/allRoutes/colors.mjs
+++ server/allRoutes/colors.mjs
@@ -8,21 +8,11 @@ const router = express.Router();
 
 router.delete('/viahex/:hex', requireAuth, viaHexDelete)
 
-// router.put('/updateColors', updateColors)
-
-// router.put('/anotherUpdateColors', anotherUpdateColors)
-
 router.get('/getColors', requireAuth, getColors)
 
 router.post('/postColors', requireAuth, postColors)
 
 router.delete('/deleteColor/:id', requireAuth, deleteColorById)
-
-// router.put('/updateColor/:id', updateColorById)
-
-// router.put('/updateAllColors', updateAllColors)
-
-// router.delete('/c/:colorValue', deleteColorByValue)
 
 
 export default router
